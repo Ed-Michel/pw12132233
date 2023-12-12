0438eea8acae9d70e7064756acd8820d377d2e23
edgar <e.michel000@gmail.com>
2023-12-12 11:12:37 -0700

    Fin de la materia (it's over)

---
+++ crudnodejs/crudvue/src/views/Grupos.vue
@@ -0,0 +1,16 @@
+<template>
+    <grupos-lista></grupos-lista>
+</template>
+
+<script>
+import GruposLista from '../components/Grupos/GruposLista.vue'
+    export default {
+        name: "Grupos",
+        components:{ GruposLista },
+        data(){
+            return{}
+        },
+        mounted(){},
+        methods:{}
+    }
+</script>
